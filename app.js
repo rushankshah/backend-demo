bc4910b46a7d61581487e99c2abf179c0ff0b3dd
Rushank Shah <rushankshah65@gmail.com>
2020-12-30 14:46:03 +0530

    Some changes

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello world!, This is Rushank\'s api')
 })
 
+app.get('/secret', (req, res) => {
+    res.send('Congratulations, you have discovered a secret api')
+})
+
 app.listen(PORT, () => {
     console.log('Server is up and running on port: '+PORT)
 })
